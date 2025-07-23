0bab2f2ea9f87222be31e432cbe07eb4cf5b7ce7
420-Paliwal <harshpaliwal0420@gmail.com>
2025-07-24 02:23:29 +0530

    add recusion and 1 to n

--- recursion/basic.py
+++ recursion/basic.py
@@ -7,3 +7,12 @@ class Solution:
         self.printNumbers(n-1)
         print(n)
 
+# This is a simple recursive function to print numbers from n to 1.
+class Solution:
+    def printNumbers(self, n):
+        # Your code goes here
+        if (n <= 0):
+          return
+        print(n)
+        self.printNumbers(n-1)
+
