ac0d6d232cb0492fcbfb4adde200d7fb1e82c0f1
420-Paliwal <harshpaliwal0420@gmail.com>
2025-07-26 01:39:54 +0530

    hashing 2

--- hashing/basic.py
+++ hashing/basic.py
@@ -6,12 +6,7 @@ def check(arr, n, x):
     ans = temp[x]
     return ans
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,5 ,5]
-n = len(arr)
-x = 5
-# result = check(arr, n, 111)
-# print(f"The number {x} appears {result} times in the array.")
-
+# This code checks how many times a character appears in a string using a simple counting method.
 def char_str(s, x):
     temp = [0 for i in range(27)]
     for i in s:
@@ -20,8 +15,3 @@ def char_str(s, x):
         temp[ord(i) - 97] += 1
     ans = temp[ord(x) - 97]
     return ans
-
-s = "hello world"
-x = 'l'
-ans = char_str(s, x)
-print(f"The character '{x}' appears {ans} times in the string '{s}'.")
