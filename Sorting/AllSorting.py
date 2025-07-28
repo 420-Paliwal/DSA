94206fe24baf0482bef59e3eff29b807cdc61b3d
420-Paliwal <harshpaliwal0420@gmail.com>
2025-07-29 04:04:34 +0530

    sorting

--- Sorting/AllSorting.py
+++ Sorting/AllSorting.py
@@ -23,3 +23,15 @@ class Solution:
             if swap == False:
                 return arr
         return arr
+    
+# Insertion Sort Implementation    
+class Solution:
+    def insertionSort(self, arr):
+        # code here
+        n = len(arr)
+        for i in range(n):
+            j = i
+            while(j > 0 and arr[j] < arr[j - 1]):
+                arr[j], arr[j-1] = arr[j-1],arr[j]
+                j -= 1
+        return arr
