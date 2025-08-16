8ef74ca2450cc8ad0335bcccf6fe74527e7cad50
420-Paliwal <harshpaliwal0420@gmail.com>
2025-08-16 11:28:30 +0530

    update easy o array

--- array/easy.py
+++ array/easy.py
@@ -86,3 +86,69 @@ class Solution:
         for i in range(len(temp1), n):
             arr[i] = 0
         return arr
+    
+# Union of two arrays
+class Solution:
+    def unionArray(self, nums1, nums2):
+        dic = set(nums1)
+        for i in nums2:
+            if i not in dic:
+                dic.add(i)
+        return list(dic)      
+
+# Find the missing number in an array of n elements
+class Solution:
+    def missingNumber(self, arr):
+        n = len(arr)
+        summ = (n*(n+1))//2
+        arr_summ = 0
+        for i in arr:
+            arr_summ += i 
+        return summ - arr_summ 
+    
+# Find the maximum number of consecutive 1's in a binary array    
+class Solution:
+    def findMaxConsecutiveOnes(self, arr):
+        maxx = 0
+        i = 0
+        n = len(arr)
+        for j in range(n):
+            if arr[j] == 1:
+                i += 1
+                if i > maxx:
+                    maxx = i
+            else:
+                i = 0
+        return maxx
+            
+# Find the single number in an array where every other number appears twice
+class Solution:
+    def singleNumber(self, nums):
+        #your code goes here
+        dic = {}
+        for i in nums:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+        for i in dic:
+            if dic[i] == 1:
+                return i 
+
+#find the length of the longest subarray with sum equal to k
+class Solution:
+    def longestSubarray(self, arr, k):
+        n = len(arr)
+        i = 0
+        maxlen = 0
+        summ = 0
+        for j in range(n):
+            summ += arr[j]
+            while ( summ > k):
+                summ -= arr[i]
+                i += 1
+            
+            if (summ == k and j-i+1 > maxlen):
+                maxlen = j-i+1
+            
+        return maxlen
