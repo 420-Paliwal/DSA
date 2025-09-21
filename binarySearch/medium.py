219b06b6c8a1a88d227bf11d6f5954c4e35326a7
420-Paliwal <harshpaliwal0420@gmail.com>
2025-09-21 22:03:58 +0530

    add roated sorted array 2

--- binarySearch/medium.py
+++ binarySearch/medium.py
@@ -1,3 +1,4 @@
+# 81. Search in Rotated Sorted Array II
 class Solution:
     def search(self, arr , target):
         low = 0
@@ -17,3 +18,28 @@ class Solution:
                 else:
                     high = mid - 1
         return -1 
+
+#search in rotated sorted array with duplicates
+class Solution(object):
+    def search(self, arr, target):
+        low = 0
+        high = len(arr)-1
+        while(low <= high):
+            mid = (low+high)//2
+            if arr[mid] == target:
+                return True
+            if arr[low] == arr[mid] == arr[high]:
+                low += 1
+                high -= 1
+                continue
+            if arr[low] <= arr[mid]:
+                if arr[low] <= target < arr[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else: 
+                if arr[mid] < target <= arr[high]:
+                    low = mid + 1 
+                else:
+                    high = mid - 1
+        return False 
